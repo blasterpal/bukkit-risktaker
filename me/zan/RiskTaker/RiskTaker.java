07e11116b0cbedce97b28a9ca4f715ce86d1601a
Hank Beaver <dev+hank@blinqmedia.com>
2012-11-24 17:21:55 -0500

    First working version 0.1, Zan approved,etc

--- me/zan/RiskTaker/RiskTaker.java
+++ me/zan/RiskTaker/RiskTaker.java
@@ -9,18 +9,21 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 
 public final class RiskTaker extends JavaPlugin implements Listener {
 
 	private static final Logger log = Logger.getLogger("Minecraft");
-
+	public static final int RANDOM_RANGE = 14;
+	
 	public void onEnable() {
 		log.info("[RiskTaker] enabled...");
 		getServer().getPluginManager().registerEvents(this, this);
@@ -30,46 +33,63 @@ public final class RiskTaker extends JavaPlugin implements Listener {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
 	{
-		if(commandLabel.equalsIgnoreCase("RiskTaker")){
-			//Now let's get random
-			Random ran = new Random();
-			int x = ran.nextInt(10) + 1;
+		if(commandLabel.equalsIgnoreCase("Risk")){
+			int x = randomInt();
 			if (!(sender instanceof Player)) {
-				sender.sendMessage("This command can only be run by a player." + Integer.toString(x));
+				sender.sendMessage("This command can only be run by a player. " + Integer.toString(x));
 			} else {
 				Player player = (Player) sender;
-				//do something
-				//Now let's get random
 				((Player) sender).sendMessage(ChatColor.BLUE + "You have taken a risk and might be rewarded! ... [" + Integer.toString(x) + "]");
 				PlayerInventory inventory = player.getInventory(); // The player's inventory
 
-				switch (x) {
-				case 1:
-					setOnFire(player);
-					break;
-				case 2: 
-					blowUp(player);
-					break;
-
-				default:
-					giveSomething(player,inventory);
-					break;
+				/* 
+				 * 1 chance /15 diamond + strike 
+				 * 3 chance /15 fire 
+				 * 3 chance /15 explode
+				 * 8 chance /15 random item
+				 */
+				
+				if (x == 1) {
+					givePrecious(player,inventory);
 				}
+				else if (x > 1 && x<5) {
+					setOnFire(player);
+				}
+				else if (x > 4 && x < 8) {
+					blowUp(player);
+				}
+				else {
+					giveSomethingRandom(player,inventory);
+				}
+
 			}
 		}
 		return true;
 	}
 
 
-
-	private void giveSomething(Player player, PlayerInventory inventory){
-		ItemStack itemstack = new ItemStack(Material.DIAMOND, 12); // A stack of diamonds
+// This method will randomly give a material/block
+	private void giveSomethingRandom(Player player, PlayerInventory inventory){
+		log.info("Gave " + player.getDisplayName()  + " something random!");
+		List <Material> list = Arrays.asList(Material.values());
+		Collections.shuffle(list);
+		Material item = list.get(0);
+		log.info("Material is " + item.name());
+		ItemStack itemstack = new ItemStack(item, 1);
+		inventory.addItem(itemstack);
+	}
+	
+// This method will give diamonds and lightning strike
+	private void givePrecious(Player player, PlayerInventory inventory){
+		ItemStack itemstack = new ItemStack(Material.DIAMOND, 3); // A stack of diamonds
 		inventory.addItem(itemstack);
 		log.info("Gave " + player.getDisplayName()  + " dimaonds!");
+		player.getWorld().strikeLightning(player.getLocation());
 	}
 
 	private void setOnFire(Player player){
 		player.setFireTicks(10000);
+		player.setHealth(5);
 		log.info("Setting " + player.getDisplayName()  + " on fire!!!");
 	}
 
@@ -78,5 +98,12 @@ public final class RiskTaker extends JavaPlugin implements Listener {
 		player.getWorld().createExplosion(player.getLocation(), explosionPower);
 		player.setHealth(1);
 		log.info("Blowing " + player.getDisplayName() + " up!!!");
+	}
+	
+	private int randomInt(){
+		//Now let's get random
+		Random ran = new Random();
+		int x = ran.nextInt(RANDOM_RANGE ) + 1; //1-RANDOM_RANGE
+		return x;
 	}
 }
